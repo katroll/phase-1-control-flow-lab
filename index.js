bba95eb7d254ef2631c3b87c57d544e68b607887
Katherine Roll <kroll256@gmail.com>
2021-11-22 16:45:38 -0500

    commit tree ed0ec4154d3562b858f583c304a871e129c859b6

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if(rideLength > 2500) {
+    return "No can do."
+  } else if(rideLength < 400) {
+    return "This one is on me!"
+  } else if (rideLength > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+
+  switch(tip) {
+    case "generous":
+      response = 'Thank you so much.';
+      return response;
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      return response;
+      break;
+    default:
+      response = 'Bye.';
+      return response;
+      break;
+
+  }
 }
